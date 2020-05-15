279ab0e6240f7e15c1b5d641b424657472a74fa6
lepaleo <palaiocho@csd.auth.gr>
2020-05-15 20:29:28 +0300

    Home frag

--- app/src/main/java/com/example/sub_tracker/Homepage.java
+++ app/src/main/java/com/example/sub_tracker/Homepage.java
@@ -12,11 +12,20 @@ import com.google.android.material.internal.NavigationMenu;
 import com.google.android.material.navigation.NavigationView;
 
 public class Homepage extends AppCompatActivity {
-
+    final Fragment frag1=new HomeFrag();
+    final Fragment frag2=new SearchFrag();
+    final Fragment frag3=new SubsFrag();
+    final Fragment frag4=new SettingsFrag();
+    Fragment active = frag1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage2);
+
+        getSupportFragmentManager().beginTransaction().add(R.id.frag_contain,frag4,"4").hide(frag4).commit();
+        getSupportFragmentManager().beginTransaction().add(R.id.frag_contain,frag3,"3").hide(frag3).commit();
+        getSupportFragmentManager().beginTransaction().add(R.id.frag_contain,frag2,"2").hide(frag2).commit();
+        getSupportFragmentManager().beginTransaction().add(R.id.frag_contain,frag1,"1").commit();
         BottomNavigationView bottom_nav =findViewById(R.id.bottom_nav_bar);
         bottom_nav.setOnNavigationItemSelectedListener(nav_listener);
     }
@@ -25,22 +34,28 @@ public class Homepage extends AppCompatActivity {
             new BottomNavigationView.OnNavigationItemSelectedListener() {
                 @Override
                 public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
-                    Fragment selected_frag=null;
 
-                    switch (menuItem.getItemId()){
-                        case R.id.nav_home:
-                            selected_frag=new HomeFrag();
-                            break;
-                        case R.id.nav_search:
-                            selected_frag=new SearchFrag();
-                            break;
-                        case R.id.nav_sub:
-                            selected_frag=new SubsFrag();
-                            break;
+                    if(menuItem.getItemId()==R.id.nav_home){
+                        getSupportFragmentManager().beginTransaction().hide(active).show(frag1).commit();
+                        active=frag1;
+                        return true;
+                    }else if (menuItem.getItemId()==R.id.nav_search){
+                        getSupportFragmentManager().beginTransaction().hide(active).show(frag2).commit();
+                        active=frag2;
+                        return true;
+                    }else if (menuItem.getItemId()==R.id.nav_sub){
+                        getSupportFragmentManager().beginTransaction().hide(active).show(frag3).commit();
+                        active=frag3;
+                        return true;
+                    }else if (menuItem.getItemId()==R.id.nav_settings){
+                        getSupportFragmentManager().beginTransaction().hide(active).show(frag4).commit();
+                        active=frag4;
+                        return true;
                     }
 
-                    getSupportFragmentManager().beginTransaction().replace(R.id.frag_contain,selected_frag).commit();
-                    return true;
+
+
+                    return false;
                 }
             };
 }
