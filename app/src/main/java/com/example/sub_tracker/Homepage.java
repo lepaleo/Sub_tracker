e1fcb1862e0f72cc49b44a0b710794ad1dff95f2
lepaleo <palaiocho@csd.auth.gr>
2020-05-15 18:35:58 +0300

    Nav nar ready with fragments

--- app/src/main/java/com/example/sub_tracker/Homepage.java
+++ app/src/main/java/com/example/sub_tracker/Homepage.java
@@ -1,31 +1,46 @@
 package com.example.sub_tracker;
 
-import android.os.Bundle;
-
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
-
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
+import androidx.fragment.app.Fragment;
 
-import android.view.View;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.internal.NavigationMenu;
+import com.google.android.material.navigation.NavigationView;
 
 public class Homepage extends AppCompatActivity {
-    Loginpage obj=new Loginpage();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_homepage);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        setContentView(R.layout.activity_homepage2);
+        BottomNavigationView bottom_nav =findViewById(R.id.bottom_nav_bar);
+        bottom_nav.setOnNavigationItemSelectedListener(nav_listener);
     }
 
+    private BottomNavigationView.OnNavigationItemSelectedListener nav_listener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+                    Fragment selected_frag=null;
+
+                    switch (menuItem.getItemId()){
+                        case R.id.nav_home:
+                            selected_frag=new HomeFrag();
+                            break;
+                        case R.id.nav_search:
+                            selected_frag=new SearchFrag();
+                            break;
+                        case R.id.nav_sub:
+                            selected_frag=new SubsFrag();
+                            break;
+                    }
+
+                    getSupportFragmentManager().beginTransaction().replace(R.id.frag_contain,selected_frag).commit();
+                    return true;
+                }
+            };
 }
