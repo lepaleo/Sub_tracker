14bad22837938393cee26d78d05d29f6bec32d81
lepaleo <palaiocho@csd.auth.gr>
2020-05-21 20:42:27 +0300

    Profile/Notifications

--- app/src/main/java/com/example/sub_tracker/Homepage.java
+++ app/src/main/java/com/example/sub_tracker/Homepage.java
@@ -8,14 +8,11 @@ import android.os.Bundle;
 import android.view.MenuItem;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
-import com.google.android.material.internal.NavigationMenu;
-import com.google.android.material.navigation.NavigationView;
 
 public class Homepage extends AppCompatActivity {
     final Fragment frag1=new HomeFrag();
-    final Fragment frag2=new SearchFrag();
-    final Fragment frag3=new SubsFrag();
-    final Fragment frag4=new SettingsFrag();
+    final Fragment frag2=new NotificationsFrag();
+    final Fragment frag3=new SettingsFrag();
     Fragment active = frag1;
 
     @Override
@@ -23,7 +20,7 @@ public class Homepage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage2);
 
-        getSupportFragmentManager().beginTransaction().add(R.id.frag_contain,frag4,"4").hide(frag4).commit();
+
         getSupportFragmentManager().beginTransaction().add(R.id.frag_contain,frag3,"3").hide(frag3).commit();
         getSupportFragmentManager().beginTransaction().add(R.id.frag_contain,frag2,"2").hide(frag2).commit();
         getSupportFragmentManager().beginTransaction().add(R.id.frag_contain,frag1,"1").commit();
@@ -43,17 +40,13 @@ public class Homepage extends AppCompatActivity {
                         getSupportFragmentManager().beginTransaction().hide(active).show(frag1).commit();
                         active=frag1;
                         return true;
-                    }else if (menuItem.getItemId()==R.id.nav_search){
+                    }else if (menuItem.getItemId()==R.id.nav_notifications){
                         getSupportFragmentManager().beginTransaction().hide(active).show(frag2).commit();
                         active=frag2;
                         return true;
-                    }else if (menuItem.getItemId()==R.id.nav_sub){
+                    }else if (menuItem.getItemId()==R.id.nav_settings){
                         getSupportFragmentManager().beginTransaction().hide(active).show(frag3).commit();
                         active=frag3;
-                        return true;
-                    }else if (menuItem.getItemId()==R.id.nav_settings){
-                        getSupportFragmentManager().beginTransaction().hide(active).show(frag4).commit();
-                        active=frag4;
                         return true;
                     }
 
--- app/src/main/java/com/example/sub_tracker/NotificationsFrag.java
+++ app/src/main/java/com/example/sub_tracker/NotificationsFrag.java
@@ -9,10 +9,10 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
-public class SearchFrag extends Fragment {
+public class NotificationsFrag extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.frag_search,container,false);
+        return inflater.inflate(R.layout.frag_notifications,container,false);
     }
 }
