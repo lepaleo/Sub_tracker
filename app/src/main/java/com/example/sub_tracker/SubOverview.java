c7f8816e323bc59e7bbfcf15021f42b7dc68293d
polyziog <polyziog@csd.auth.gr>
2020-06-02 19:02:20 +0300

    last corrected

--- app/src/main/java/com/example/sub_tracker/SubOverview.java
+++ app/src/main/java/com/example/sub_tracker/SubOverview.java
@@ -10,6 +10,7 @@ import android.widget.Button;
 public class SubOverview extends AppCompatActivity {
 
     private Button backButton;
+    private Button editButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,6 +27,20 @@ public class SubOverview extends AppCompatActivity {
                 startActivity(backactivity);
                 finish();
                 overridePendingTransition(R.anim.slide_in_left,R.anim.slide_out_right);
+            }
+        });
+
+
+        editButton = findViewById(R.id.editbutton);
+
+        //editbutton to edit subcsciption activity
+        editButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent edit = new Intent(SubOverview.this, EditSubscription.class);
+                startActivity(edit);
+                finish();
+                overridePendingTransition(R.anim.slide_in_right,R.anim.slide_out_left);
             }
         });
 
